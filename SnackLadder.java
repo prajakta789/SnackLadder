588d098bfb9ed9ca69237fb69746f4a44c4dfb9b
prajakta <prajaktamahajan789@gmail.com>
2021-09-24 22:56:49 +0530

    Total number of times dice win the game

--- SnackLadder.java
+++ SnackLadder.java
@@ -4,41 +4,47 @@ import java.util.Random;
 
 public class SnackLadder {
 
-	public static void main(String[] args) {
-		int position=0;
-		int IS_LADDER = 2;
-		int IS_SNAKE = 1;
-		int NO_PLAY = 0;
-		int WINNING_POSITION = 100;
+	 public static void main(String[] args) {
+		 
+	        int position = 0;
+	        int IS_NO_PLAY = 0;
+	        int IS_LADDER = 2;
+	        int IS_SNAKE = 1;
+	        int WINNING_POSITION = 100;
+	        int diceCount = 0;
 
+	        
 
-		while (position < 100) {
-			Random random = new Random();
-			int diceNumber = random.nextInt(6) + 1;
-			int option = random.nextInt(3);
-
-			if (option == IS_LADDER) {
-				System.out.println("Player position is ladder");
-				position += diceNumber;
-
-				if (position > 100) {
-					position -= diceNumber;
-				}
-
-			} else if (option == IS_SNAKE) {
-				System.out.println("Player is snack");
-				position -= diceNumber;
-
-				if (position < 0) {
-					position = 0;
-				}
-
-			} else {
-				System.out.println("Player position is no play");
-			}
-			System.out.println("Dice Number : " + diceNumber);
-			System.out.println("Position : " + position);
-		}
+	        while (position < WINNING_POSITION) {
+	            diceCount++;
+	            Random random = new Random();
+	            int diceNumber = random.nextInt(6) + 1;
+	            int option = random.nextInt(3);
+	            
+	            if (option == IS_LADDER) {
+	                System.out.println("Player position at ladder");
+	                position += diceNumber;
+	                
+	                if (position > 100){
+	                    position -= diceNumber;
+	                }
+	                
+	            } else if (option == IS_SNAKE) {
+	                System.out.println("Player position at snack");
+	                position -= diceNumber;
+	                
+	                if (position < 0) {
+	                    position = 0;
+	                }
+	            }
+	            
+	            else {
+	                System.out.println("Player position is no play");
+	            }
+	            
+	         
+	            System.out.println("Position : " + position);
+	            System.out.println("Dice Count : " + diceCount);
+	        }
+	    }
 	}
-}
-
