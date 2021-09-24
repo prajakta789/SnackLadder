475a08e41e283892c75c4ee5f665041d5c7f018b
prajakta <prajaktamahajan789@gmail.com>
2021-09-24 20:49:05 +0530

    Checks for a options player are no play,ladder or snack

--- SnackLadder.java
+++ SnackLadder.java
@@ -3,12 +3,27 @@ package com.bridgelabz.Snack_Ladder;
 import java.util.Random;
 
 public class SnackLadder {
+	 public static void main(String[] args) {
+	        int position = 0;
+	        int IS_LADDER = 2;
+	        int IS_SNAKE = 1;
+	        int NO_PLAY = 0;
 
-	public static void main(String[] args) {
-
-		System.out.println("Plyers rolls the die");
-		Random random = new Random();
-		int diceNumber = random.nextInt(6)+1;
-		System.out.println("Dice Number : " + diceNumber);
+	        Random random = new Random();
+	        int diceNumber = random.nextInt(6)+1;
+	        int option = random.nextInt(3);
+	        if (option == IS_LADDER){
+	            System.out.println("Player position is ladder");
+	            position += diceNumber;
+	        }
+	        else if (option == IS_SNAKE){
+	            System.out.println("Player position is snack");
+	            position -= diceNumber;
+	        }
+	        else {
+	            System.out.println("Player position is No play");
+	        }
+	        System.out.println("Dice Number : " + diceNumber);
+	        System.out.println("Position : " + position);
+	    }
 	}
-}
