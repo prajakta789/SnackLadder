4876489383d2147cd05ab5099f96cfa5ee9a2e54
prajakta <prajaktamahajan789@gmail.com>
2021-09-24 16:26:44 +0530

    Plyer rolls the die to get a number between 1 to 6

--- SnackLadder.java
+++ SnackLadder.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.Snack_Ladder;
 
+import java.util.Random;
+
 public class SnackLadder {
 
 	public static void main(String[] args) {
-        int position = 0;
-        System.out.println("single player at start position is: "+ position);
+
+		System.out.println("Plyers rolls the die");
+		Random random = new Random();
+		int diceNumber = random.nextInt(6)+1;
+		System.out.println("Dice Number : " + diceNumber);
 	}
-	
 }
