196dc701d850dc1d64cfd97f80fc0b12c43ff04e
prajakta <prajaktamahajan789@gmail.com>
2021-09-24 15:46:17 +0530

    Player at start position is

---
+++ SnackLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Snack_Ladder;
+
+public class SnackLadder {
+
+	public static void main(String[] args) {
+        int position = 0;
+        System.out.println("single player at start position is: "+ position);
+	}
+	
+}
